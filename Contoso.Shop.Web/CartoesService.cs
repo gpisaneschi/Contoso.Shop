19b5309b968c5f2abb1d05bd4f4ca8b2ccfc7ae9
Gino <pisaneschi.gino@gmail.com>
2017-02-15 13:07:40 -0200

    Primeiro commit

---
+++ Contoso.Shop.Web/CartoesService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contoso.Shop.Web
+{
+    public class CartoesService 
+    {
+        private readonly ILogger logger;
+        private int count = 0;
+
+        public CartoesService(ILogger<CartoesService> logger)
+        {
+            this.logger = logger;
+        }
+
+        public int Salvar(string ccnumber, string cvv)
+        {
+            logger.LogInformation($"Cartao cadastrado. {ccnumber}:{cvv}");
+
+            return ++count;
+        }
+    }
+}
